67dfb0ff5a0abf2a65e4a0568ac432e1e870e119
vladimir-kraus <vladimir.kraus@gmail.com>
2016-01-17 15:32:16 +0100

    added 'apply' and 'reduce' as free functions

--- numcube/__init__.py
+++ numcube/__init__.py
@@ -2,4 +2,5 @@ from numcube.version import __version__
 from numcube.axis import Axis
 from numcube.cube import Cube, concatenate, stack, apply2
 from numcube.index import Index
+from numcube.functions import apply, reduce
 
--- numcube/functions.py
+++ numcube/functions.py
@@ -1,4 +1,11 @@
-from numcube.cube import Cube
+
+
+def apply(cube, func, *args):
+    return cube.apply(func, *args)
+
+
+def reduce(cube, func, axis=None, keep=None, group=None, sort_grp=True):
+    return cube.reduce(func, axis, keep, group, sort_grp)
 
 
 def intersect(cubes):
